900dacc0fdd412a19d3bb70b811aeba200b3ad4a
Aaron <aaronbee1990@gmail.com>
2024-07-31 09:52:19 -0400

    finished dynamic list

--- JavaScript/07 Manipulating Documents/A dynamic shopping list/main.js
+++ JavaScript/07 Manipulating Documents/A dynamic shopping list/main.js
@@ -1,27 +1,27 @@
 const input = document.querySelector("#input");
 const list = document.querySelector("ul");
 const addButton =  document.querySelector(".add");
-const delButton = document.querySelector(".delete");
 addButton.addEventListener("click",addItem);
-delButton.addEventListener("click",deleteItem);
-let id = 0
+
 
 function addItem(){
    const item = document.createElement("li");
-   item.setAttribute("id",id)
 
    const delButton = document.createElement("button");
-   delButton.setAttribute("class","delete");
-   delButton.setAttribute("id",id);
    delButton.textContent = "Delete";
+   delButton.addEventListener("click",deleteItem);
+   
+   function deleteItem(){
+      item.remove();
+   }
    
    item.textContent = input.value;
    item.appendChild(delButton);
    list.appendChild(item);
    
-   id++;
-}
-
-function deleteItem(){
+   input.value=""
+   input.focus();
+   
    
 }
+
